58d92a32b067a09071e91bf79e7dfcbd6f762b91
wodo96 <renzo.cuomo1996@gmail.com>
2022-10-16 20:07:36 +0200

    Final Commit

--- Homework_3/Form1.Designer.cs
+++ Homework_3/Form1.Designer.cs
@@ -32,13 +32,18 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 12);
+            this.button1.Location = new System.Drawing.Point(364, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 29);
+            this.button1.Size = new System.Drawing.Size(96, 22);
             this.button1.TabIndex = 0;
             this.button1.Text = "Compute CSV";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +51,12 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 47);
+            this.richTextBox1.Location = new System.Drawing.Point(10, 48);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.MaximumSize = new System.Drawing.Size(450, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(236, 379);
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(450, 500);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -58,25 +66,77 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(10, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(82, 22);
             this.button2.TabIndex = 2;
             this.button2.Text = "Select CSV";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(492, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(128, 244);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(683, 129);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(347, 419);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(492, 312);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(128, 50);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(683, 48);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(347, 50);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Select \"Destination\" or \"protocol\" to see how many packets the source sent to an " +
+    "address or which is the most used protocol.";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1058, 571);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,5 +146,9 @@
         private RichTextBox richTextBox1;
         private OpenFileDialog openFileDialog1;
         private Button button2;
+        private ListBox listBox1;
+        private RichTextBox richTextBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- Homework_3/Form1.cs
+++ Homework_3/Form1.cs
@@ -34,6 +34,7 @@ namespace Homework_3
                                 this.tables[i].Add(field);
                                 this.headers.Add(field);
                                 i++;
+                                this.richTextBox1.AppendText(field + " | ");
                             }
                         }
                         else
@@ -43,19 +44,79 @@ namespace Homework_3
                                 this.tables.Add(new List<string>());
                                 this.tables[i].Add(field);
                                 i++;
+                                this.richTextBox1.AppendText(field + " | ");
                             }
                         }
+                        this.richTextBox1.AppendText("\n");
                         i = 0;
-                        this.richTextBox1.AppendText("riuscito\n");
                     }
                 }
             }
+            this.textBox2.Visible = true;
+            this.richTextBox2.Visible = true;
+            this.textBox1.Visible = true;
+            this.textBox1.Text = ("There are " + (this.tables.First().Count()-1) + " packets found!");
+            this.listBox1.Visible = true;
+            this.listBox1.DataSource = headers;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private List<string> allLower(List<string> temp)
+        {
+            List<string> list = new List<string>();
+            foreach (string field in temp)
+            {
+                list.Add(field.ToLower());
+            }
+            return list;
+        }
+
+        private void compute_row(List<string> tempList)
+        {
+            List<string> currentRow = allLower(tempList);
+            currentRow.RemoveAt(0);
+            List<List<string>> rows = new List<List<string>>();
+            foreach (string row in currentRow)
+            {
+                if (!(rows.Exists(e => e.Contains(row))))
+                {
+                    rows.Add(currentRow.FindAll(s => s.Equals(row)));
+                }
+            }
+
+            foreach (List<string> elem in rows)
+            {
+                if (elem.First() != "")
+                {
+                    this.richTextBox2.AppendText("\"" + elem.First() + "\" occurs: " + elem.Count() + " time\n");
+                }
+                else if (elem.Count() > 1)
+                {
+                    this.richTextBox2.AppendText("There are " + elem.Count() + " blank values.\n");
+                }
+                else
+                {
+                    this.richTextBox2.AppendText("There is " + elem.Count() + " blank value.\n");
+                }
+            }
+
+        }
+            private void button2_Click(object sender, EventArgs e)
         {
             this.openFileDialog1.ShowDialog();
             this.fileName = this.openFileDialog1.FileName;
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = this.listBox1.SelectedIndex;
+            List<string> elem_list = this.tables.ElementAt(i);
+            this.richTextBox2.Text = "";
+            /*foreach (string field in elem_list)
+            {
+                this.richTextBox1.AppendText(field + " \n");
+            }*/
+            compute_row(elem_list);
+        }
+
     }
 }
